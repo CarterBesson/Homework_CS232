1463ceb45bc72e1915f4873072bce8c0de5ec66c
CarterBesson <bessca01@pfw.edu>
2023-01-25 00:28:47 -0500

    hw2

---
+++ hw2/primes.c
@@ -0,0 +1,48 @@
+/* File: primes.c */
+/* Author: Britton Wolfe */
+/* Date: July 16, 2009 */
+/* This program outputs all the primes in the range given */
+/* by the command line arguments */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+int main(int argc, const char** argv){
+
+    int lowerBound, upperBound;
+
+    if(argc != 3){
+        fprintf(stderr, "USAGE: %s lowerBound upperBound\n", argv[0]);
+        return -1;
+    }
+  
+    lowerBound = atoi(argv[1]);
+    upperBound = atoi(argv[2]);
+  
+    if(lowerBound < 1 || upperBound < 1){
+        fprintf(stderr, "ERROR: both the lowerBound (%d) and the upperBound (%d)"
+	          " must be positive.\n", lowerBound, upperBound);
+        return -2;
+    }
+
+    int i,j;
+    for (i = lowerBound; i <= upperBound; i++){
+        for (j = 2; j <= i; j++){
+            if (j == i){
+                printf("%d \n",i);
+            }
+            else if (i % j == 0){
+                break;
+            }
+        }
+    }
+    
+    
+ 
+    
+    
+    
+
+    return 0;
+}
